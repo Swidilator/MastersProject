85b7fd26f914d1929e352f40966e9f97c94e7a82
Kyle Weiher <kyleweiher@gmail.com>
2019-06-30 19:28:05 +0200

    Initial testing of perceptual loss

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import torchvision
 import torch
 from matplotlib import pyplot as plt
 
+from PerceptualLoss import PerceptualDifference, PerceptualLossNetwork
+
 from CRN import RefinementModule
 
 if __name__ == "__main__":
-    rm = RefinementModule(3, 10, 3, torch.Size([20, 3, 32, 32]), False)
-    input_test = torch.FloatTensor(4, 6, 32, 32).uniform_(0, 255)
+    rm = RefinementModule(61, 64, 3, torch.Size([20, 3, 64, 64]), False)
+    # noinspection PyArgumentList
+    input_test = torch.FloatTensor(20, 64, 64, 64).uniform_(0, 255)
     output_test = rm(input_test)
 
     plt.figure(0)
@@ -16,4 +19,9 @@ if __name__ == "__main__":
     plt.figure(1)
     plt.imshow(output_test[0, 0, :].detach().numpy())
     plt.show()
+
+    loss_holder: PerceptualLossNetwork = PerceptualLossNetwork()
+    loss = loss_holder.layer_list[0](output_test) - loss_holder.layer_list[0](input_test)
+    loss_sum = loss.sum()
+    loss_sum.backward()
 
