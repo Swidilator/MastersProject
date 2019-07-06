a1fde04c7906995b974d6333e1a163c186d43625
Kyle Weiher <kyleweiher@gmail.com>
2019-07-06 10:33:44 +0200

    Rename Dataloader.py to Data_Management.py, general testing changes in Main.py

---
+++ Main.py
@@ -0,0 +1,53 @@
+
+import torchvision
+import torch
+from matplotlib import pyplot as plt
+from Data_Management import CRNDataset
+
+from CRN import RefinementModule
+
+if __name__ == "__main__":
+
+    # data_set = torchvision.datasets.Cityscapes(root="C:\\Users\\Kyle\\Desktop\\Train\\",
+    #                                            split="train",
+    #                                            mode="fine",
+    #                                            target_type="semantic",
+    #                                            # target_transform=transforms
+    #                                            )
+    data_set = CRNDataset(
+        max_input_height_width=(32, 64),
+        root="C:\\Users\\Kyle\\Desktop\\Train\\",
+        split="train",
+    )
+    a, b = data_set[0]
+
+    print(size)
+
+    data_loader = torch.utils.data.DataLoader(data_set,
+                                              batch_size=1,
+                                              shuffle=True,
+                                              num_workers=1)
+
+    image, smnt = next(iter(data_loader))
+    print(type(image))
+
+    #plt.imshow(torchvision.transforms.ToPILImage(image[0].detach().numpy()))
+    plt.show()
+
+    rm = RefinementModule(
+        prior_layer_channel_count=0,
+        semantic_input_channel_count=3,
+        output_channel_count=3,
+        input_height_width=(32, 64),
+        is_final_module=False
+    )
+
+    output_test = rm(smnt[5])
+
+    # plt.figure(0)
+    # plt.imshow(torchvision.transforms.ToPILImage(smnt[0]))
+    # plt.show()
+    # plt.figure(1)
+    # plt.imshow(output_test[0].detach().numpy())
+    # plt.show()
+
