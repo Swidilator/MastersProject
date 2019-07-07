2754e19de9c4c2191a5c7cc07dc1ffa49c567568
Kyle Weiher <kyleweiher@gmail.com>
2019-07-07 23:53:49 +0200

    Comment out old code in CRN network

--- CRN.py
+++ CRN.py
@@ -82,39 +82,39 @@ class RefinementModule(modules.Module):
             x = self.leakyReLU(x)
         return x
 
-
+# TODO Fill with actual code, currently old nextwork
 class CRN(torch.nn.Module):
     def __init__(self, num_classes=1000):
         super(CRN, self).__init__()
-        self.features = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),  # 0
-            torch.nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),  # 3
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),  # 6
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),  # 1
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),  # 4
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),  # 6
-        )
+        # self.features = torch.nn.Sequential(
+        #     torch.nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),  # 0
+        #     torch.nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        #     nn.Conv2d(64, 192, kernel_size=5, padding=2),  # 3
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        #     nn.Conv2d(192, 384, kernel_size=3, padding=1),  # 6
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(384, 256, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(256, 256, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        # )
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(),
+        #     nn.Linear(256 * 6 * 6, 4096),  # 1
+        #     nn.ReLU(inplace=True),
+        #     nn.Dropout(),
+        #     nn.Linear(4096, 4096),  # 4
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(4096, num_classes),  # 6
+        # )
 
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
-        x = self.classifier(x)
+        # x = self.features(x)
+        # x = x.view(x.size(0), 256 * 6 * 6)
+        # x = self.classifier(x)
         return x
 
 
