3e33c4dd1f3c3b9f9595638fc2d7c4c3b68ac555
Kyle Weiher <kyleweiher@gmail.com>
2019-07-07 23:54:59 +0200

    Complete one-hot encoding of masks

--- Data_Management.py
+++ Data_Management.py
@@ -1,9 +1,10 @@
+import torch
 from torch.utils.data import DataLoader, Dataset
+from torch.nn.functional import one_hot
 from torchvision.datasets import Cityscapes
 from torchvision import transforms
 from PIL import Image
 from matplotlib import pyplot as plt
-import torch
 
 
 class CRNDataset(Dataset):
@@ -12,6 +13,7 @@ class CRNDataset(Dataset):
         max_input_height_width: tuple,
         root: str,
         split: str,
+        num_classes: int
         # mode: str,
         # target_type: str
     ):
@@ -20,24 +22,37 @@ class CRNDataset(Dataset):
         #     mean=[0.485, 0.456, 0.406],
         #     std=[0.229, 0.224, 0.225]
         # )
+        self.num_classes = num_classes
         self.dataset: Cityscapes = Cityscapes(
             root=root, split=split, mode="fine", target_type="semantic"
         )
-        self.transforms: list = []
+        self.image_resize_transforms: list = []
+        self.mask_resize_transforms: list = []
 
-        scale_lambda = lambda x: x * 255
-
-        curr_size = 1
+        curr_size = 2
         while curr_size < max_input_height_width[1]:
 
             curr_size = curr_size * 2
             print(curr_size)
-            self.transforms.append(
+            self.image_resize_transforms.append(
                 transforms.Compose(
                     [
-                        transforms.Resize((int(curr_size / 2), curr_size)),
+                        transforms.Resize(
+                            (int(curr_size / 2), curr_size), Image.BILINEAR
+                        ),
                         transforms.ToTensor(),
-                        transforms.Lambda(scale_lambda),
+                    ]
+                )
+            )
+            self.mask_resize_transforms.append(
+                transforms.Compose(
+                    [
+                        transforms.Resize(
+                            (int(curr_size / 2), curr_size), Image.NEAREST # NEAREST as the values are categories and are not continuous
+                        ),
+                        transforms.ToTensor(),
+                        transforms.Lambda(lambda x: (x * 255).long()[0]),
+                        transforms.Lambda(lambda x: one_hot(x, self.num_classes)),
                     ]
                 )
             )
@@ -47,21 +62,10 @@ class CRNDataset(Dataset):
         masks = []
         img, msk = self.dataset.__getitem__(index)
         tens = transforms.ToTensor()
-        msk_tens = tens(msk)
-        msk_tens_ids = (msk_tens * 255).int()
-        for i in range(len(self.transforms)):
-            images.append(self.transforms[i](img))
-            masks.append(self.transforms[i](msk))
-
-        size = masks[1].shape
+        for i in range(len(self.image_resize_transforms)):
+            images.append(self.image_resize_transforms[i](img))
+            masks.append(self.mask_resize_transforms[i](msk))
         return images, masks
 
     def __len__(self):
         return self.dataset.__len__()
-
-
-def onehot_encode_target(target: torch.Tensor, num_classes: int):
-    one_hot_tensor = torch.zeros
-    for i in range(num_classes):
-        # TODO
-        pass
