2dea436cb8e1d98e391fcee1dcd805d38fdb1fce
Kyle Weiher <kyleweiher@gmail.com>
2019-07-07 18:02:13 +0200

    Begin working on one-hot encoding input

--- Data_Management.py
+++ Data_Management.py
@@ -1,16 +1,19 @@
 from torch.utils.data import DataLoader, Dataset
 from torchvision.datasets import Cityscapes
 from torchvision import transforms
+from PIL import Image
+from matplotlib import pyplot as plt
+import torch
 
 
 class CRNDataset(Dataset):
     def __init__(
-            self,
-            max_input_height_width: tuple,
-            root: str,
-            split: str,
-            # mode: str,
-            # target_type: str
+        self,
+        max_input_height_width: tuple,
+        root: str,
+        split: str,
+        # mode: str,
+        # target_type: str
     ):
         super(CRNDataset, self).__init__()
         # normalize = transforms.Normalize(
@@ -18,12 +21,11 @@ class CRNDataset(Dataset):
         #     std=[0.229, 0.224, 0.225]
         # )
         self.dataset: Cityscapes = Cityscapes(
-            root=root,
-            split=split,
-            mode="fine",
-            target_type="semantic",
+            root=root, split=split, mode="fine", target_type="semantic"
         )
         self.transforms: list = []
+
+        scale_lambda = lambda x: x * 255
 
         curr_size = 1
         while curr_size < max_input_height_width[1]:
@@ -34,7 +36,8 @@ class CRNDataset(Dataset):
                 transforms.Compose(
                     [
                         transforms.Resize((int(curr_size / 2), curr_size)),
-                        transforms.ToTensor()
+                        transforms.ToTensor(),
+                        transforms.Lambda(scale_lambda),
                     ]
                 )
             )
@@ -44,17 +47,11 @@ class CRNDataset(Dataset):
         masks = []
         img, msk = self.dataset.__getitem__(index)
         tens = transforms.ToTensor()
-        mat = tens(msk)
-        big_mat = mat * 24
-        print("msk shape:", big_mat.shape)
-        print(big_mat[0, 230:300, 512])
+        msk_tens = tens(msk)
+        msk_tens_ids = (msk_tens * 255).int()
         for i in range(len(self.transforms)):
-            images.append(
-                self.transforms[i](img)
-            )
-            masks.append(
-                self.transforms[i](msk)
-            )
+            images.append(self.transforms[i](img))
+            masks.append(self.transforms[i](msk))
 
         size = masks[1].shape
         return images, masks
@@ -63,5 +60,8 @@ class CRNDataset(Dataset):
         return self.dataset.__len__()
 
 
-
-
+def onehot_encode_target(target: torch.Tensor, num_classes: int):
+    one_hot_tensor = torch.zeros
+    for i in range(num_classes):
+        # TODO
+        pass
