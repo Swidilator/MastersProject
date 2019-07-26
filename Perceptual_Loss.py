8890cf7affe4dfa480fe9a65cbe5308d5025f25e
Kyle Weiher <kyleweiher@gmail.com>
2019-07-26 13:43:44 +0200

    Add forward hooks to perceptual loss' vgg network, storing activations in-layer

--- Perceptual_Loss.py
+++ Perceptual_Loss.py
@@ -6,6 +6,12 @@ from torchvision.transforms import Resize
 from copy import copy
 
 
+def get_layer_values(self: torch.nn.modules.conv.Conv2d, input: tuple, output: torch.Tensor) -> None:
+    # input is a tuple of packed inputs
+    # output is a Tensor. output.data is the Tensor we are interested
+    self.stored_output: torch.Tensor = output
+
+
 class PerceptualDifference(torch.autograd.Function):
     @staticmethod
     def forward(ctx, a, b):
@@ -21,18 +27,25 @@ class PerceptualLossNetwork(modules.Module):
     def __init__(self):
         super(PerceptualLossNetwork, self).__init__()
 
-        vgg = torchvision.models.vgg19(pretrained=True, progress=True)
-        self.vgg_1 = copy(vgg.features[2])  # 64
-        self.vgg_1.requires_grad = False
-        self.vgg_2 = copy(vgg.features[7])  # 128
-        self.vgg_2.requires_grad = False
-        self.vgg_3 = copy(vgg.features[12])  # 256
-        self.vgg_3.requires_grad = False
-        self.vgg_4 = copy(vgg.features[21])  # 512
-        self.vgg_4.requires_grad = False
-        self.vgg_5 = copy(vgg.features[30])  # 512
-        self.vgg_5.requires_grad = False
-        del vgg
+        self.vgg = torchvision.models.vgg19(pretrained=True, progress=True)
+
+        self.loss_layer_numbers: tuple = (2, 7, 12, 21, 30)
+
+        for i, num in enumerate(self.loss_layer_numbers):
+            self.vgg.features[num].register_forward_hook(get_layer_values)
+            self.vgg.features[num].requires_grad = False
+
+        # self.vgg_1 = copy(vgg.features[2])  # 64
+        # self.vgg_1.requires_grad = False
+        # self.vgg_2 = copy(vgg.features[7])  # 128
+        # self.vgg_2.requires_grad = False
+        # self.vgg_3 = copy(vgg.features[12])  # 256
+        # self.vgg_3.requires_grad = False
+        # self.vgg_4 = copy(vgg.features[21])  # 512
+        # self.vgg_4.requires_grad = False
+        # self.vgg_5 = copy(vgg.features[30])  # 512
+        # self.vgg_5.requires_grad = False
+        # del vgg
 
     def forward(self, input, truth):
         result_1 = PerceptualDifference(self.vgg_1(input), self.vgg_1(truth))
