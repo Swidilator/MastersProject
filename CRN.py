c87635064e38a756752701420ee554bce9362ce7
Kyle Weiher <kyleweiher@gmail.com>
2019-07-28 17:05:36 +0200

    In CRN, add CRNFramework class conforming to MastersModel framework, removed unneeded imports, changed types to custom type-hinting types

--- CRN.py
+++ CRN.py
@@ -2,9 +2,141 @@ import torch
 import torch.nn as nn
 import torch.nn.modules as modules
 from torch.nn.functional import one_hot
-import torchvision.transforms as transforms
-from PIL import Image
 from math import log2
+
+from Data_Types import image_size, epoch_output
+from Data_Management import CRNDataset
+from Perceptual_Loss import PerceptualLossNetwork
+from Training_Framework import MastersModel
+
+
+class CRNFramework(MastersModel):
+    def __init__(
+        self,
+        device: torch.device,
+        data_path: str,
+        input_tensor_size: image_size,
+        max_input_height_width: image_size,
+        num_output_images: int,
+        num_classes: int,
+        batch_size: int,
+        num_loader_workers: int,
+    ):
+        super(CRNFramework, self).__init__(device=device)
+        self.batch_size = batch_size
+        self.input_tensor_size = input_tensor_size
+
+        self.__set_data_loader__(
+            data_path,
+            max_input_height_width,
+            num_classes,
+            batch_size,
+            num_loader_workers,
+        )
+        self.__set_model__(
+            input_tensor_size, max_input_height_width, num_output_images, num_classes
+        )
+
+    def __set_data_loader__(
+        self,
+        data_path,
+        max_input_height_width,
+        num_classes,
+        batch_size,
+        num_loader_workers,
+    ):
+        self.__data_set__ = CRNDataset(
+            max_input_height_width=max_input_height_width,
+            root="../CityScapes Samples/Train/",
+            split="train",
+            num_classes=num_classes,
+        )
+
+        self.data_loader: torch.utils.data.DataLoader = torch.utils.data.DataLoader(
+            self.__data_set__,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_loader_workers,
+        )
+
+    def __set_model__(
+        self, input_tensor_size, max_input_height_width, num_output_images, num_classes
+    ) -> None:
+        self.crn: CRN = CRN(
+            input_tensor_size=input_tensor_size,
+            final_image_size=max_input_height_width,
+            num_output_images=num_output_images,
+            num_classes=num_classes,
+        )
+        self.crn = self.crn.to(self.device)
+
+        self.optimizer = torch.optim.SGD(self.crn.parameters(), lr=0.01, momentum=0.9)
+        self.loss_net: PerceptualLossNetwork = PerceptualLossNetwork()
+        self.loss_net = self.loss_net.to(self.device)
+
+    def save_model(self, model_dir: str) -> None:
+        pass
+
+    def load_model(self, model_dir: str, model_name: str) -> None:
+        pass
+
+    def train(self) -> epoch_output:
+        self.crn.train()
+        torch.cuda.empty_cache()
+        loss_sum: float = 0.0
+        for batch_idx, (img, msk) in enumerate(self.data_loader):
+            self.optimizer.zero_grad()
+            img: torch.Tensor = img.to(self.device)
+            msk: torch.Tensor = msk.to(self.device)
+            noise: torch.Tensor = torch.randn(
+                self.batch_size,
+                1,
+                self.input_tensor_size[0],
+                self.input_tensor_size[1],
+                device=self.device,
+            )
+            noise = noise.to(self.device)
+
+            out = self.crn(inputs=[msk, noise, self.batch_size])
+
+            out = CRNFramework.__normalise__(out)
+
+            loss: torch.Tensor = self.loss_net([out, img])
+            loss.backward()
+            loss_sum += loss.item()
+            self.optimizer.step()
+            del loss, msk, noise, img
+        return loss_sum, None
+
+    def eval(self) -> epoch_output:
+        pass
+
+    @staticmethod
+    def __single_channel_normalise__(
+        channel: torch.Tensor, params: tuple
+    ) -> torch.Tensor:
+        # channel = [H ,W]   params = (mean, std)
+        return (channel - params[0]) / params[1]
+
+    @staticmethod
+    def __single_image_normalise__(image: torch.Tensor, mean, std) -> torch.Tensor:
+        for i in range(3):
+            image[i] = CRNFramework.__single_channel_normalise__(
+                image[i], (mean[i], std[i])
+            )
+        return image
+
+    @staticmethod
+    def __normalise__(input: torch.Tensor) -> torch.Tensor:
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+
+        if len(input.shape) == 4:
+            for i in range(input.shape[0]):
+                input[i] = CRNFramework.__single_image_normalise__(input[i], mean, std)
+        else:
+            input = CRNFramework.__single_image_normalise__(input, mean, std)
+        return input
 
 
 class RefinementModule(modules.Module):
@@ -123,15 +255,15 @@ class RefinementModule(modules.Module):
 class CRN(torch.nn.Module):
     def __init__(
         self,
-        input_tensor_size: tuple,
-        final_image_size: tuple,
+        input_tensor_size: image_size,
+        final_image_size: image_size,
         num_output_images: int,
         num_classes: int,
     ):
         super(CRN, self).__init__()
 
-        self.input_tensor_size: tuple = input_tensor_size
-        self.final_image_size: tuple = final_image_size
+        self.input_tensor_size: image_size = input_tensor_size
+        self.final_image_size: image_size = final_image_size
         self.num_output_images: int = num_output_images
         self.num_classes: int = num_classes
 
