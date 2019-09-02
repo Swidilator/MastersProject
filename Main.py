4a0b22881147d51eef1987e4b386a4a91bedfa36
Kyle Weiher <kyleweiher@gmail.com>
2019-09-02 18:35:39 +0200

    Commented and reorganised Main file for better clarity

--- Main.py
+++ Main.py
@@ -3,35 +3,38 @@ import os
 from matplotlib import pyplot as plt
 
 from CRN.CRN import CRNFramework
+from GAN.GAN import GANFramework
 from Helper_Stuff import *
 import wandb
 
 # from torchviz import make_dot
 
 if __name__ == "__main__":
+    # Determinism
     torch.manual_seed(0)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+    # General Model Settings
     MAX_INPUT_HEIGHT_WIDTH: tuple = (128, 256)
-    INPUT_TENSOR_SIZE: tuple = (4, 8)
-    NUM_OUTPUT_IMAGES: int = 1
     NUM_CLASSES: int = 35
     NUM_INNER_CHANNELS = 512
     BATCH_SIZE: int = 2
     HISTORY_LEN: int = 100
 
+    # CRN
+    INPUT_TENSOR_SIZE: tuple = (4, 8)
+    NUM_OUTPUT_IMAGES: int = 1
+
+    # System Settings
     PREFER_CUDA: bool = True
     NUM_LOADER_WORKERS: int = 6
     MODEL_PATH: str = "./Models/"
-
     CITYSCAPES_PATH: str = os.environ["CITYSCAPES_PATH"]
-    print("Dataset path: {cityscapes}".format(cityscapes=CITYSCAPES_PATH))
-
-    TRAINING_MACHINE: str = os.environ["TRAINING_MACHINE"]
-    print("Training Machine: {machine}".format(machine=TRAINING_MACHINE))
-
     DATA_PATH: str = CITYSCAPES_PATH
+    TRAINING_MACHINE: str = os.environ["TRAINING_MACHINE"]
+    print("Dataset path: {cityscapes}".format(cityscapes=CITYSCAPES_PATH))
+    print("Training Machine: {machine}".format(machine=TRAINING_MACHINE))
 
     if PREFER_CUDA:
         if torch.cuda.is_available():
@@ -44,27 +47,38 @@ if __name__ == "__main__":
         device = torch.device("cpu")
         print("Device: CPU")
 
-    model_frame: CRNFramework = CRNFramework(
+    # Run Specific Settings
+    TRAIN: tuple = (False, 100)
+    SAMPLE: tuple = (True, 1)
+    WANDB: bool = False
+    SAVE_EVERY_EPOCH: bool = True
+    LOAD_BEFORE_TRAIN: bool = False
+    # CRN
+    UPDATE_PL_LAMBDAS: bool = True
+
+    # Choose Model
+
+    # model_frame: CRNFramework = CRNFramework(
+    #     device=device,
+    #     data_path=DATA_PATH,
+    #     input_tensor_size=INPUT_TENSOR_SIZE,
+    #     max_input_height_width=MAX_INPUT_HEIGHT_WIDTH,
+    #     num_output_images=NUM_OUTPUT_IMAGES,
+    #     num_inner_channels=NUM_INNER_CHANNELS,
+    #     num_classes=NUM_CLASSES,
+    #     batch_size=BATCH_SIZE,
+    #     num_loader_workers=NUM_LOADER_WORKERS,
+    #     history_len=HISTORY_LEN,
+    # )
+
+    model_frame: GANFramework = GANFramework(
         device=device,
         data_path=DATA_PATH,
-        input_tensor_size=INPUT_TENSOR_SIZE,
         max_input_height_width=MAX_INPUT_HEIGHT_WIDTH,
-        num_output_images=NUM_OUTPUT_IMAGES,
-        num_inner_channels=NUM_INNER_CHANNELS,
         num_classes=NUM_CLASSES,
         batch_size=BATCH_SIZE,
         num_loader_workers=NUM_LOADER_WORKERS,
-        history_len=HISTORY_LEN,
     )
-
-    TRAIN: tuple = (True, 100)
-    SAMPLE: tuple = (False, 20)
-    WANDB: bool = True
-    SAVE_EVERY_EPOCH: bool = True
-    LOAD_BEFORE_TRAIN: bool = False
-
-    # CRN
-    UPDATE_PL_LAMBDAS: bool = True
 
     # Training
     if TRAIN[0]:
@@ -101,7 +115,7 @@ if __name__ == "__main__":
 
     # Sampling
     if SAMPLE[0]:
-        model_frame.load_model(MODEL_PATH)
+        # model_frame.load_model(MODEL_PATH)
         img_list: sample_output = model_frame.sample(SAMPLE[1])
         for i, img in enumerate(img_list):
             print(img_list[i])
