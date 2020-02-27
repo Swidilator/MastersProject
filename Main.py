676472b0856d17a4fbb0cbd143cfc92699eb0bfd
Kyle Weiher <kyleweiher@gmail.com>
2020-02-27 16:13:10 +0200

    Fix / allow the use of the <Model Name>_Latest.pt save again

--- Main.py
+++ Main.py
@@ -126,7 +126,10 @@ if __name__ == "__main__":
         raise SystemExit
 
     if args["load_saved_model"]:
-        model_frame.load_model(args["model_save_dir"], args["load_saved_model"])
+        if args["load_saved_model"] == "Latest":
+            model_frame.load_model(args["model_save_dir"])
+        else:
+            model_frame.load_model(args["model_save_dir"], args["load_saved_model"])
 
     if not args["wandb"]:
         os.environ["WANDB_MODE"] = "dryrun"
