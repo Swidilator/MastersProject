455de983c170d47a2b17289367de7211519f6506
Kyle <kyleweiher@gmail.com>
2020-12-05 18:29:04 +0200

    Initial commit for ResNetBlock.py.

---
+++ support_scripts/components/ResNetBlock.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+
+from support_scripts.components import RMBlock
+
+
+class ResNetBlock(nn.Module):
+    def __init__(
+        self,
+        channel_count: int,
+        input_height_width: tuple,
+    ):
+        super().__init__()
+        self.rm_block_1 = RMBlock(
+            channel_count // 2,
+            channel_count,
+            input_height_width,
+            1,
+            "weight",
+            1,
+        )
+
+        self.rm_block_2 = RMBlock(
+            channel_count // 2,
+            channel_count // 2,
+            input_height_width,
+            3,
+            "weight",
+            32,
+        )
+
+        self.rm_block_3 = RMBlock(
+            channel_count,
+            channel_count // 2,
+            input_height_width,
+            1,
+            "weight",
+            1,
+        )
+
+    def forward(self, x: torch.Tensor):
+        out = self.rm_block_1(x)
+        out = self.rm_block_2(out, "before")
+        out = self.rm_block_3(out, "before")
+        return out + x
