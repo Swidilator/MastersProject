1bc40c96bf925e1e851ccf594da6643f7d92ab4d
Kyle <kyleweiher@gmail.com>
2020-12-05 18:33:07 +0200

    Update init files.

--- support_scripts/components/__init__.py
+++ support_scripts/components/__init__.py
@@ -1,6 +1,7 @@
 
-__all__ = ["Block", "RMBlock", "FeatureEncoder", "FlowNetWrapper"]
+__all__ = ["Block", "RMBlock", "FeatureEncoder", "FlowNetWrapper", "ResNetBlock"]
 
 from support_scripts.components.blocks import Block, RMBlock
 from support_scripts.components.feature_encoder import FeatureEncoder
 from support_scripts.components.FlowNet import FlowNetWrapper
+from support_scripts.components.ResNetBlock import ResNetBlock
